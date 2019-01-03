930a575368bcd974b797a92d6a78b33dbe4da253
Marcin Horczak <marcin.horczak737@gmail.com>
2019-01-03 23:04:06 +0100

    Changed webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = (env = {}) => {
         output: {
             path: path.join(__dirname, bundleOutputDir),
             filename: '[name].js',
-            publicPath: '/'
+            publicPath: './'
         },
         module: {
             rules: [
