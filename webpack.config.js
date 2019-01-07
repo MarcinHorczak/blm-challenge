3da589f1715a180f6e1291e7bd43681411342c56
Marcin Horczak <marcin.horczak@flighscope.com.pl>
2019-01-07 09:21:59 +0100

    Fixed webpack.config.js and package.json files

--- webpack.config.js
+++ webpack.config.js
@@ -5,67 +5,45 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env = {}) => {
-
     const isDevBuild = !env.production;
-    const bundleOutputDir = env.bundleOutputDir;
     const templateInjectBefore  = env.templateInjectBefore;
     const templateFileName = env.templateFileName || 'index.html';
-
-    const apiHost = env.apiHost || 'localhost';
-    const apiPort = env.apiPort ? Number(env.apiPort) : 3000;
-
-    const apiProtocol = env.apiProtocol || 'http';
-    const apiUrl = `${apiProtocol}://${apiHost}:${apiPort}/api`;
-    console.info(`Using api: ${apiUrl}`);
-
-    if (!(bundleOutputDir && String(bundleOutputDir))) {
-        throw new Error(`
-            Invalid bundle output dir.
-            Ensure the parameter --env.bundleOutputDir is set.
-            Expected string but "${bundleOutputDir}" is given.
-        `)
-    }
 
     return [{
         devServer: {
             historyApiFallback: {
                 index: "/"
-            }
+            },
         },
         mode: isDevBuild ? 'development' : 'production',
         stats: { modules: false },
         entry: { 'main': './src/index.tsx' },
         resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
         output: {
-            // path: path.join(__dirname, bundleOutputDir),
-            path: path.resolve(`build`),
+            path: path.join(__dirname, 'build'),
             filename: '[name].js',
             publicPath: './'
         },
         module: {
             rules: [
-                { test: /\.tsx?$/, loader: 'tslint-loader', enforce: 'pre', options: { emitErrors: isDevBuild } },
+                { test: /\.tsx?$/, loader: 'tslint-loader', enforce: 'pre', options: {  emitErrors: true } },
                 { test: /\.tsx?$/, use: 'ts-loader?silent=true' },
                 {
                     test: /\.scss$/,
-                    test: /\.s?css$/,
                     use: [
                         isDevBuild
                             ? 'style-loader'
                             : MiniCssExtractPlugin.loader,
                         'css-loader',
                         'sass-loader',
-                      ],
+                    ],
                 },
                 { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' },
-                { test: /\.(woff2?|svg|ttf)$/, use: 'file-loader', exclude: __dirname + '/src/assets/images' }
+                { test: /\.json$/, use: 'json-loader', use: 'json-loader' }
             ]
         },
         plugins: 
             [
-                new webpack.DefinePlugin({
-                    API_URL: JSON.stringify(apiUrl),
-                }),
                 new MiniCssExtractPlugin({
                     filename: isDevBuild ? '[name].css' : '[name].[hash].css',
                     chunkFilename: isDevBuild ? '[id].css' : '[id].[hash].css',
@@ -78,12 +56,12 @@ module.exports = (env = {}) => {
                         templateInjectBefore
                     }
                 }),
-                new CleanWebpackPlugin([bundleOutputDir], { allowExternal: true } ),
+                new CleanWebpackPlugin(['build'], { allowExternal: true } ),
             ].concat(
                 isDevBuild ? [
                     new webpack.SourceMapDevToolPlugin({
                         filename: '[file].map',
-                        moduleFilenameTemplate: path.relative(bundleOutputDir, '[resourcePath]')
+                        moduleFilenameTemplate: path.relative('build', '[resourcePath]')
                     })
                 ] : []
             )
